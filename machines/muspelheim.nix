52ffc3ae13c51258cc5c5771cca6d05ef6d61008
Romain Ernandez <romain.ernandez@gmail.com>
2018-11-02 15:05:20 +0100

    add mtn yelster public

--- machines/muspelheim.nix
+++ machines/muspelheim.nix
@@ -27,4 +27,13 @@
   nix.maxJobs = lib.mkDefault 4;
 
   networking.hostName = "muspelheim";
+
+  fileSystems."/mnt/yelster_public" = {
+      device = "//192.168.70.2/public";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      in ["${automount_opts},guest,user=nobody,vers=1.0"];
+  };
 }
